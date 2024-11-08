d980ec45f58a71bdfd898e4b81c7b839d643992e
skomposzczet <72391558+skomposzczet@users.noreply.github.com>
2024-11-08 23:10:22 +0100

    config tmux

--- home/modules/tmux.nix
+++ home/modules/tmux.nix
@@ -8,7 +8,7 @@
     clock24 = true;
     prefix = "C-a";
     shell = "${pkgs.zsh}/bin/zsh";
-    extraConfig = /* tmux */ ''
+    extraConfig = /* bash */ ''
         bind '"' split-window -v -c "#{pane_current_path}"
         bind % split-window -h -c "#{pane_current_path}"
 
@@ -22,7 +22,20 @@
     plugins = with pkgs; [ 
       {
         plugin = tmuxPlugins.catppuccin;
-        extraConfig = "set -g @catppuccin_flavour 'mocha'";
+        extraConfig = /* bash */ ''
+            set -g @catppuccin_flavour 'mocha'
+
+            set -g @catppuccin_window_default_text "#W"
+            set -g @catppuccin_window_current_text "#W"
+
+            set -g @catppuccin_status_left_separator  ""
+            set -g @catppuccin_status_right_separator " "
+            set -g @catppuccin_status_fill "all"
+            set -g @catppuccin_status_connect_separator "yes"
+
+            set -g status-left ""
+            set -gF status-right "#{E:@catppuccin_status_application}#{E:@catppuccin_status_session}"
+        '';
       }
       {
         plugin = tmuxPlugins.yank;
